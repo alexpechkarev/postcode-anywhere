ebba6eeb31ae016ea0694aa2c6680e5ff4e396f9
Alexander PEchkarev <alexpechkarev@gmail.com>
2015-08-02 15:47:13 +0100

    adding phpunit test

--- tests/PATest.php
+++ tests/PATest.php
@@ -1,15 +1,61 @@
 <?php
 
-class PATest extends TestCase {
+class PATest extends PHPUnit_Framework_TestCase {
+        
 
-        /**
-         * Can class be instantiated
-         */
-        public function testInstance(){
-            $inst = new \PostcodeAnywhere\PostcodeAnywhere();
-            
-            $this->assertInstanceOf('\PostcodeAnywhere\PostcodeAnywhere', $inst);
-        }
-        /***/  
+
+    protected $app;
+    
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function __construct()
+    {
+            $this->app = require __DIR__.'/../bootstrap/app.php';
+
+            $this->app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+    }
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+            if ( ! $this->app)
+            {
+                    $this->refreshApplication();
+            }
+    }
+
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+            if ($this->app)
+            {
+                    $this->app->flush();
+            }
+    }    
+    
+    
+    
+    /**
+     * Can class be instantiated
+     */
+    public function testInstance(){
+        $inst = new \PostcodeAnywhere\PostcodeAnywhere();
+
+        $this->assertInstanceOf('\PostcodeAnywhere\PostcodeAnywhere', $inst);
+    }
+    /***/
+               
 
 }
