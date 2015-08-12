d8a98f92a8a42d412dc020f6dac4b1f04cc7747a
Alexander PEchkarev <alexpechkarev@gmail.com>
2015-08-12 19:29:48 +0100

    Adding PHP plain version and tests

--- tests/PATest.php
+++ tests/PATest.php
@@ -1,10 +1,10 @@
 <?php
 
+
 class PATest extends PHPUnit_Framework_TestCase {
-        
 
-
-    protected $app;
+    
+    protected $config;
     
     /**
      * Creates the application.
@@ -13,40 +13,94 @@ class PATest extends PHPUnit_Framework_TestCase {
      */
     public function __construct()
     {
-
-
+        $this->config = include(__DIR__.'/../config/postcodeanywhere.php');
+        
     }
 
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
-    public function setUp()
-    {
 
-    }
-
-    /**
-     * Clean up the testing environment before the next test.
-     *
-     * @return void
-     */
-    public function tearDown()
-    {
-
-    }    
-    
     
     
     /**
      * Can class be instantiated
      */
     public function testInstance(){
-        
-        $this->assertTrue(TRUE);
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( $this->config );
+        $this->assertInstanceOf('\PostcodeAnywhere\php\PostcodeAnywhere', $pa);
     }
     /***/
+    
+     /**
+     * @expectedException ErrorException
+     */
+    public function testConfigException()
+    {
+        $pa = new \APostcodeAnywhere\php\PostcodeAnywhere( [] );
+
+    } 
+    
+     /**
+     * @expectedException ErrorException
+     */
+    public function testParamsException()
+    {
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( ['params' =>[] ] );
+
+    } 
+    
+     /**
+     * @expectedException ErrorException
+     */
+    public function testParamsKeyException()
+    {
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( ['params' =>['key' => ''] ] );
+    } 
+    
+     /**
+     * @expectedException ErrorException
+     */
+    public function testUrlException()
+    {
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( ['url' => ''] );
+    } 
+    
+     /**
+     * @expectedException ErrorException
+     */
+    public function testServicesException()
+    {
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( ['services' => ''] );
+    } 
+    
+     /**
+     * @expectedException ErrorException
+     */
+    public function testEndpointsException()
+    {
+
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( ['endpoints' => ''] );
+    } 
+       
+    
+    
+    public function testArrayHasKey(){
+        
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( $this->config );
+        $this->assertNull($pa->serviceUrl);
+        
+    }
+    
+    
+    public function testSetRequestType(){
+        
+        $pa = new \PostcodeAnywhere\php\PostcodeAnywhere( $this->config );
+        
+        $pa->setRequestType([0 =>'find']);
+        $this->assertEquals('find', $pa->requestType);
+        
+        $pa->setRequestType([0 => 'retrieve']);
+        $this->assertEquals('retrieve', $pa->requestType); 
+        
+    }
                
 
 }
